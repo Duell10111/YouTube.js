958ea08aaa58bbe3d749753db88d60e722928e7f
Konstantin Späth <duell10111@t-online.de>
2024-11-09 01:43:28 +0100

    fix: fix lockup-view-videos

--- src/parser/classes/LockupView.ts
+++ src/parser/classes/LockupView.ts
@@ -3,20 +3,21 @@ import { Parser, type RawNode } from '../index.js';
 import CollectionThumbnailView from './CollectionThumbnailView.js';
 import LockupMetadataView from './LockupMetadataView.js';
 import NavigationEndpoint from './NavigationEndpoint.js';
+import ThumbnailView from './ThumbnailView.js';
 
 export default class LockupView extends YTNode {
   static type = 'LockupView';
 
-  content_image: CollectionThumbnailView | null;
+  content_image: CollectionThumbnailView | ThumbnailView | null;
   metadata: LockupMetadataView | null;
   content_id: string;
-  content_type: 'SOURCE' | 'PLAYLIST' | 'ALBUM' | 'PODCAST' | 'SHOPPING_COLLECTION' | 'SHORT' | 'GAME' | 'PRODUCT';
+  content_type: 'SOURCE' | 'PLAYLIST' | 'ALBUM' | 'PODCAST' | 'SHOPPING_COLLECTION' | 'SHORT' | 'GAME' | 'PRODUCT' | 'VIDEO';
   on_tap_endpoint: NavigationEndpoint;
 
   constructor(data: RawNode) {
     super();
 
-    this.content_image = Parser.parseItem(data.contentImage, CollectionThumbnailView);
+    this.content_image = Parser.parseItem(data.contentImage, [ CollectionThumbnailView, ThumbnailView ]);
     this.metadata = Parser.parseItem(data.metadata, LockupMetadataView);
     this.content_id = data.contentId;
     this.content_type = data.contentType.replace('LOCKUP_CONTENT_TYPE_', '');
