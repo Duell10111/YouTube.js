403fd40b8b3f0d6cd581aa2725288622c856f14a
Konstantin Späth <duell10111@t-online.de>
2025-01-04 20:23:22 +0100

    chore: add shelf headers for new tv endpoints

--- src/parser/classes/TileHeader.ts
+++ src/parser/classes/TileHeader.ts
@@ -4,16 +4,17 @@ import Thumbnail from './misc/Thumbnail.js';
 import ThumbnailOverlayTimeStatus from './ThumbnailOverlayTimeStatus.js';
 import TileMetadata from './TileMetadata.js';
 import ThumbnailOverlayIcon from './ThumbnailOverlayIcon.js';
+import ThumbnailOverlayResumePlayback from './ThumbnailOverlayResumePlayback.js';
 
 export default class TileHeader extends YTNode {
   static type = 'TileHeader';
 
   thumbnail: Thumbnail[];
-  thumbnail_overlays: ObservedArray<ThumbnailOverlayTimeStatus | ThumbnailOverlayIcon | TileMetadata> | null;
+  thumbnail_overlays: ObservedArray<ThumbnailOverlayTimeStatus | ThumbnailOverlayIcon | ThumbnailOverlayResumePlayback | TileMetadata> | null;
 
   constructor(data: RawNode) {
     super();
     this.thumbnail = Thumbnail.fromResponse(data.thumbnail);
-    this.thumbnail_overlays = Parser.parse(data.thumbnailOverlays, true, [ ThumbnailOverlayTimeStatus, ThumbnailOverlayIcon, TileMetadata ]);
+    this.thumbnail_overlays = Parser.parse(data.thumbnailOverlays, true, [ ThumbnailOverlayTimeStatus, ThumbnailOverlayIcon, ThumbnailOverlayResumePlayback, TileMetadata ]);
   }
 }
